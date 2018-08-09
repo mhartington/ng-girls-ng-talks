7cef898e631a5bf181d0b25e6211cee5942ea9ab
mhartington <mikehartington@gmail.com>
2018-08-09 10:57:22 +0300

    final

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { TodoService } from '../todo/todo.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  constructor(
+    private todoService: TodoService,
+    private alertCtrl: AlertController
+  ) {}
 
+  get todos() {
+    return this.todoService.todos;
+  }
+
+  async addTodo() {
+    const popup = await this.alertCtrl.create({
+      header: 'Create Todo',
+      inputs: [
+        {
+          type: 'text',
+          name: 'text',
+          placeholder: 'Enter todo text'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: ({ text }) => {
+            this.todoService.addTodo({ text, completed: false });
+          }
+        }
+      ]
+    });
+    await popup.present();
+  }
+
+  removeTodo(todo){
+    this.todoService.removeTodo(todo)
+  }
+
+  completeTodo(todo){
+    this.todoService.toggleTodo(todo)
+  }
 }
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -5,13 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class TodoService {
   public todos = [
-    { text: 'buy milk', id: 0, completed: false },
-    { text: 'learn ngrx', id: 1, completed: false },
-    { text: 'go to sleep', id: 2, completed: false }
+    { text: 'Buy milk', id: 0, completed: false },
+    { text: 'Learn ngrx', id: 1, completed: false },
+    { text: 'Go to sleep', id: 2, completed: false }
   ];
   constructor() {}
 
   addTodo(todo: { text: string; completed: boolean }) {
     this.todos = [...this.todos, { ...todo, id: Date.now() }];
+  }
+
+  removeTodo(todo: { text: string; completed: boolean, id: number }){
+    this.todos = this.todos.filter(entry => entry.id !== todo.id );
+  }
+
+  toggleTodo(todo: { text: string; completed: boolean, id: number }){
+    this.todos = this.todos.map(entry => entry.id === todo.id ? {...entry, completed: !entry.completed} : entry)
   }
 }
